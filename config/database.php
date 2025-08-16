69da9dde42b32e95ff7b7818c76e2aa16c3b098f
David Poplampo <poplampodavid@gmail.com>
2025-08-16 18:37:34 +0000

    Update database.php

--- config/database.php
+++ config/database.php
@@ -8,7 +8,7 @@ class Database {
     private $host = 'localhost';
     private $db_name = 'furniture_shop';
     private $username = 'root';
-    private $password = '';
+    private $password = 'root';
     private $conn;
 
     public function getConnection() {
